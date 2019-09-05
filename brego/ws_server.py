ffce84644baa7b37cf1f9c1383113b7ee24b5eb9
Camille Scott <camille.scott.w@gmail.com>
2019-09-05 16:08:22 -0700

    add some docs and type hints

--- brego/ws_server.py
+++ brego/ws_server.py
@@ -1,43 +1,22 @@
-"""
-Curio - Concurrent I/O
+"""A simple curio websocket server. Based on the old example by
+David Beazley, but updated for the modern wsproto API. This
+also (more or less) properly handles incoming connection
+requests; the original would crash if the handler managed
+to get an outgoing message on the queue before processing
+the connection Request and accepting it.
 
-Copyright (C) 2015-2017
-David Beazley (Dabeaz LLC)
-All rights reserved.
+Adapted by Camille Scott (@camillescott), 2019.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-
-* Redistributions of source code must retain the above copyright notice,
-  this list of conditions and the following disclaimer.  
-* Redistributions in binary form must reproduce the above copyright notice, 
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.  
-* Neither the name of the David Beazley or Dabeaz LLC may be used to
-  endorse or promote products derived from this software without
-  specific prior written permission. 
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
-
-"""A Curio websocket server.
-
-pip install wsproto before running this.
+Requires wsproto:
+    pip install wsproto
 
 """
 
 import sys
+from typing import Tuple, Callable, Awaitable
+
+import curio
+import wsproto
 
 from curio import Queue, run, spawn, TaskGroup
 from curio.socket import IPPROTO_TCP, TCP_NODELAY
@@ -45,11 +24,25 @@ from wsproto import WSConnection, ConnectionType
 from wsproto.events import (AcceptConnection, CloseConnection,
                             Request, Message, TextMessage, BytesMessage)
 
-DATA_TYPES = (TextMessage, BytesMessage)
 
+async def process_incoming(wsconn: wsproto.WSConnection,
+                           in_q:   curio.Queue, 
+                           client: curio.io.Socket) -> Tuple[bool, bytes]:
 
-async def process_incoming(wsconn, in_q, client):
+    """Process incoming messages: pass text messages on to the handler's
+    incoming queue, and handle acceptance and closing.
+
+    Args:
+        wsconn (wsproto.WSConnection): Active WebSocket connection
+        in_q (curio.Queue): Queue of incoming messages hooked up to the handler
+        client (curio.Socket): The client socket
+
+    Returns:
+        Tuple[bool, bytes]: Connection closed status and server response.
+    """
+
     rsp = b''
+    closed = False
     for event in wsconn.events():
         if isinstance(event, (TextMessage, BytesMessage)):
             await in_q.put(event.data)
@@ -68,11 +61,26 @@ async def process_incoming(wsconn, in_q, client):
             rsp += wsconn.send(event.response())
         else:
             print('Unhandled event: {!r}'.format(event), file=sys.stderr)
-    return rsp
+    return closed, rsp
 
 
-async def ws_adapter(in_q, out_q, client, _):
-    """A simple, queue-based Curio-Sans-IO websocket bridge."""
+async def ws_adapter(in_q: curio.Queue,
+                     out_q: curio.Queue,
+                     client: curio.io.Socket, _):
+
+    """A queue-based WebSocket bridge. Sits between a
+    a `curio.tcp_server` and a user-defined handler; the handler
+    should accept an ingoing and outgoing queue which will be loaded
+    by the adapter.
+
+    Args:
+        in_q (curio.Queue): Queue for incoming messages.
+        out_q (curio.Queue): Queue for outgoing messages.
+        client (curio.Socket): The client socket.
+        _: dummy
+
+    Returns:
+    """
     client.setsockopt(IPPROTO_TCP, TCP_NODELAY, 1)
     wsconn = WSConnection(ConnectionType.SERVER)
     closed = False
@@ -81,7 +89,7 @@ async def ws_adapter(in_q, out_q, client, _):
     # need to accept the request first
     auth = await client.recv(65535)
     wsconn.receive_data(auth)
-    rsp = await process_incoming(wsconn, in_q, client)
+    closed, rsp = await process_incoming(wsconn, in_q, client)
     await client.sendall(rsp)
 
     while not closed:
@@ -95,7 +103,7 @@ async def ws_adapter(in_q, out_q, client, _):
 
         if task is wstask:
             wsconn.receive_data(result)
-            rsp = await process_incoming(wsconn, in_q, client)
+            closed, rsp = await process_incoming(wsconn, in_q, client)
 
             await client.sendall(rsp)
         else:
@@ -110,30 +118,51 @@ async def ws_adapter(in_q, out_q, client, _):
                 payload = wsconn.send(Message(data=result))
                 await client.sendall(payload)
                 await out_q.task_done()
-    print("Bridge done.")
 
 
-async def ws_echo_server(in_queue, out_queue):
-    """Just echo websocket messages, reversed. Echo 3 times, then close."""
+async def ws_echo_server(in_queue: curio.Queue,
+                         out_queue: curio.Queue) -> None:
+    """Example handler: echoes messages, reversed.
+
+    Args:
+        in_queue (curio.Queue):
+        out_queue (curio.Queue):
+
+    Returns:
+    """
     for _ in range(3):
+        # note: if you want your handler to push a continuous
+        # outgoing stream, you should check that the in_queue
+        # has messages before awaiting it, otherwise it will
+        # hang and never push out your data.
         msg = await in_queue.get()
         if msg is None:
             # The ws connection was closed.
             break
         await out_queue.put(msg[::-1])
-    print("Handler done.")
 
 
-def serve_ws(handler):
-    """Start processing web socket messages using the given handler."""
-    async def run_ws(client, addr):
+# Some "typedefs." Starting to wonder if this is a good idea for python...
+HandlerType = Callable[[curio.Queue, curio.Queue], Awaitable[None]]
+RunnerType  = Callable[[curio.io.Socket, Tuple[str, int]], Awaitable[None]]
+
+def serve_ws(handler: HandlerType) -> RunnerType:
+    """Build the WebSocket runner with the user-supplied handler.
+
+    Args:
+        handler (HandlerType): The handler coroutine that juggles the incoming
+                               that juggles the incoming and outgoing data queues.
+
+    Returns:
+        RunnerType: The runner coroutine.
+    """
+    async def run_ws(client: curio.io.Socket, addr: Tuple[str, int]) -> None:
         in_q, out_q = Queue(), Queue()
         ws_task = await spawn(ws_adapter, in_q, out_q, client, addr)
         await handler(in_q, out_q)
         await out_q.put(None)
         await ws_task.join()  # Wait until it's done.
         # Curio will close the socket for us after we drop off here.
-        print("Master task done.")
 
     return run_ws
 
@@ -141,5 +170,5 @@ def serve_ws(handler):
 if __name__ == '__main__':
     from curio import tcp_server
     port = 5000
-    print(f'Listening on port {port}.')
+    print(f'Listening on port {port}.', file=sys.stderr)
     run(tcp_server, '', port, serve_ws(ws_echo_server))
